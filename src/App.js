ea7efe65197f0c3bb0f8fe2b1846e3fffcb2d9b8
soumilk <soumilk.k@gmail.com>
2020-05-17 00:42:46 +0530

    All work before introducing the redux

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -19,9 +19,24 @@ class App extends React.Component {
   unsubscribeFromAuth= null;
 
   componentDidMount(){
-    auth.onAuthStateChanged(user=>{
-      this.setState({currentUser:user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth=>{
+      if (userAuth){
+        // If the user is signed in 
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot=>{
+          //console.log(snapShot.data());
+          this.setState({
+            currentUser:{
+              id:snapShot.id,
+              ...snapShot.data()
+
+            }
+          });
+        });
+      }
+      else{
+        this.setState({currentUser:userAuth});
+      }
     });
   }
 
