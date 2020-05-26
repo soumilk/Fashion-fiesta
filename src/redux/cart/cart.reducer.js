87437a32be7e51f92fb2aacd58623e56ca1bed42
soumilk <soumilk.k@gmail.com>
2020-05-26 16:08:34 +0530

    Add items to cart functionality

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import CartActionTypes from './cart.types';
-
+import {addItemToCart} from './cart.utils';
 const INITIAL_STATE= {
-  hidden: true
+  hidden: true,
+  cardItems:[]
 };
 
 const cartReducer = (state= INITIAL_STATE, action)=>{
@@ -10,7 +11,12 @@ const cartReducer = (state= INITIAL_STATE, action)=>{
       return {
         ...state,
         hidden: !state.hidden
-      }
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cardItems: addItemToCart(state.cardItems,action.payload)
+      };
     default :
     return state;
   }
