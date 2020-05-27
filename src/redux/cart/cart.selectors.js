b758414cc642152b6326b251166c083a79efd227
soumilk <soumilk.k@gmail.com>
2020-05-27 15:13:48 +0530

    Update cart checkout component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -16,3 +16,13 @@ export const selectCartItemsCount= createSelector(
   cartItems=>
   cartItems.reduce((accumulatedQuantity, cartItem)=> accumulatedQuantity+ cartItem.quantity ,0)
 );
+
+// This is the selector for the total cost of the items in the cart 
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+  cartItems.reduce(
+    (accumulatedQuantity,cartItem) =>
+      accumulatedQuantity + cartItem.quantity* cartItem.price, 0
+  )
+);
