6e103a6f99f5d2f384e88f14f7786198b42b4c5b
soumilk <soumilk.k@gmail.com>
2020-06-04 15:12:47 +0530

    Typo correction, changed caterogy to collection (nested routes)

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import'./collection.styles.scss';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+const CollectionPage = ({match}) =>(
+  <div className='category'>
+    <h2>Collection Page</h2>
+  </div>
+);
+
+export default CollectionPage;
