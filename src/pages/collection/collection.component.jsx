6e640e476e1f15f60c03c5641ca7daf4fab7a60a
soumilk <soumilk.k@gmail.com>
2020-06-04 17:08:10 +0530

    Fixing errors

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import {connect } from 'react-redux';
+import {selectCollection} from '../../redux/shop/shop.selector';
 import'./collection.styles.scss';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-const CollectionPage = ({match}) =>(
+const CollectionPage = ({collection}) =>(
   <div className='category'>
     <h2>Collection Page</h2>
   </div>
 );
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps)=>({
+  collection:selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)( CollectionPage) ;
