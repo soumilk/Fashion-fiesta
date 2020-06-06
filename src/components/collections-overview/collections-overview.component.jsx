a530df861b323a158ddced54380f0789b3d57318
soumilk <soumilk.k@gmail.com>
2020-06-06 21:02:28 +0530

    fixing some errors

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,7 +3,7 @@ import './collections-overview.styles.scss';
 import CollectionPreview  from '../preview-collection/collection-preview.component';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-import  {selectCollections} from '../../redux/shop/shop.selector';
+import  {selectCollectionsForPreview} from '../../redux/shop/shop.selector';
 
 const CollectionsOverview = ({collections}) =>(
   <div className='collections-overview'>
@@ -16,7 +16,7 @@ const CollectionsOverview = ({collections}) =>(
 );
 
 const mapStateToProps  = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview); 
