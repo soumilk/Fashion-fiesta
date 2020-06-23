4c393d66814c15a209303c0cdf4d55d23601b302
soumilk <soumilk.k@gmail.com>
2020-06-23 20:37:47 +0530

    Container pattern

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selector';
+import WithSpinner from '../with-spinner/with-spinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: selectIsCollectionFetching
+});
+
+const CollectionsOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionsOverview);
+
+// The above syntax is equivalent to this
+// connect(mapStateToProps)(WithSpinner(CollectionsOverview))
+
+export default CollectionsOverviewContainer;
